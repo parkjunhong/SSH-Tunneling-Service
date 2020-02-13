2ae8f7cf98a90e2825e275fe0feb22786e2fba33
Park_Jun_Hong <parkjunhong77@gmail.com>
2020-02-13 13:54:36 +0900

    init.

---
+++ src/main/java/open/commons/tools/ssh/ApplicationMain.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Park Jun-Hong_(parkjunhong77/google/com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *
+ * This file is generated under this project, "ssh-tunneling".
+ *
+ * Date  : 2020. 2. 13. 오후 1:53:48
+ *
+ * Author: Park_Jun_Hong_(fafanmama_at_naver_com)
+ * 
+ */
+
+package open.commons.tools.ssh;
+
+/**
+ * 
+ * @since 2020. 2. 13.
+ * @version _._._
+ * @author Park_Jun_Hong_(fafanmama_at_naver_com)
+ */
+public class ApplicationMain {
+
+    /**
+     * 
+     * @since 2020. 2. 13.
+     */
+    public ApplicationMain() {
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
