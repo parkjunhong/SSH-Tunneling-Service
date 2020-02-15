ef4f73ce7cba97de66ec02329bbb75632c5416ad
Park Jun-Hong <parkjunhong77@gmail.com>
2020-02-16 00:28:46 +0900

    update cli bash script.

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
-if [ -z $1 ];
+PROFILE=""
+DEPLOY=0
+while [ ! -z "$1" ];
+do
+	case "$1" in
+		-p | --profile)
+			shift
+			PROFILE=$1
+			;;
+		-d)
+			DEPLOY=1
+			;;
+		*)
+			;;
+	esac
+	shift
+done
+
+if [ -z $PROFILE ];
 then
-	echo "No profile."
+	echo
+	echo "Require PROFILE"
+	echo
+	echo "./build-run.sh -p <profile> -d"
+	echo " -p: Profile"
+	echo " -d: deploy after build."
 	exit 1
 fi
 
 echo
-echo " > mvn clean package -Dbuild.profile=$1 -U -up"
-mvn clean package -Dbuild.profile=$1 -U -up
+echo " > mvn clean package -Dbuild.profile=$PROFILE -U -up"
+mvn clean package -Dbuild.profile=$PROFILE -U -up
+
+if [ $DEPLOY -ne 1 ];
+then
+	exit 0
+fi
+
 echo
-echo " > cd ./deploy/$1"
-cd ./deploy/$1
+echo " > cd ./deploy/$PROFILE"
+cd ./deploy/$PROFILE
 echo " > ./deploy.sh"
 ./deploy.sh
 cd ..
